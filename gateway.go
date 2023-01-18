1902a9e5e3a1f34b2141f199513453b8734f3600
rusl222 <galeev_rusl@mail.ru>
2023-01-19 04:14:03 +0500

    version 2.0.0

--- gateway.go
+++ gateway.go
@@ -1,65 +1,108 @@
 package gateway
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/netip"
 )
 
-// --- class v1.0.0
-type Gateway struct {
-	Directions  []Direction
-	connections []Connection
+var Version string = "v2.0.0"
+
+type Network int
+
+const (
+	Udp Network = iota
+	Tcp
+)
+
+func (d Network) String() string {
+	return [...]string{"udp", "tcp"}[d]
 }
 
+type Gateway struct {
+	Client  Direction
+	Servers []Direction
+}
+
+var client connection
+var servers []connection
+
 type Direction struct {
+	Net    Network
 	Self   netip.AddrPort
 	Remote netip.AddrPort
 }
 
-type Connection struct {
+type connection struct {
 	readBuffer []byte
-	conn       *net.UDPConn
+	connUdp    *net.UDPConn
+	//connTcp    *net.TCPConn
 }
 
 func (s Gateway) Run() error {
-	for _, dir := range s.Directions {
+	for _, dir := range append(s.Servers, s.Client) {
 		if !dir.Self.IsValid() {
 			return &net.AddrError{Err: "Address not valid", Addr: dir.Self.String()}
 		}
 		if !dir.Remote.IsValid() {
 			return &net.AddrError{Err: "Address not valid", Addr: dir.Remote.String()}
 		}
+	}
 
-		log.Printf("connecting %s - %s\n", dir.Self, dir.Remote)
+	switch s.Client.Net {
+	case Udp:
+		log.Printf("Подключение %s - %s\n", s.Client.Self, s.Client.Remote)
 		conn1, err := net.DialUDP("udp",
-			net.UDPAddrFromAddrPort(dir.Self),
-			net.UDPAddrFromAddrPort(dir.Remote))
+			net.UDPAddrFromAddrPort(s.Client.Self),
+			net.UDPAddrFromAddrPort(s.Client.Remote))
 		if err != nil {
-			fmt.Printf("Не удалось создать подключение %s - %s", dir.Self, dir.Remote)
+			log.Fatalf("Не удалось подключиться! %s - %s", s.Client.Self, s.Client.Remote)
 			return &net.OpError{Op: "DialUDP", Net: "udp"}
 		}
-
-		s.connections = append(s.connections, Connection{
+		client = connection{
 			readBuffer: make([]byte, 300),
-			conn:       conn1})
+			connUdp:    conn1}
+	case Tcp:
+
 	}
 
-	for i := 1; i < len(s.connections); i++ {
-		go s.transport(s.connections[i], []Connection{s.connections[0]})
+	for _, dir := range s.Servers {
+		switch s.Client.Net {
+		case Udp:
+			log.Printf("Подключение %s - %s\n", dir.Self, dir.Remote)
+			conn1, err := net.DialUDP("udp",
+				net.UDPAddrFromAddrPort(dir.Self),
+				net.UDPAddrFromAddrPort(dir.Remote))
+			if err != nil {
+				log.Fatalf("Не удалось подключится! %s - %s", dir.Self, dir.Remote)
+			} else {
+				servers = append(servers, connection{
+					readBuffer: make([]byte, 300),
+					connUdp:    conn1})
+			}
+		case Tcp:
+
+		}
 	}
 
-	s.transport(s.connections[0], s.connections[1:])
+	for _, con1 := range servers {
+		go s.transport(con1, []connection{client})
+	}
+
+	s.transport(client, servers)
 
 	return nil
 }
 
-func (s Gateway) transport(src Connection, dst []Connection) {
+func (s Gateway) transport(src connection, dst []connection) {
 	for {
-		n, _, _ := src.conn.ReadFromUDP(src.readBuffer)
-		for _, c := range dst {
-			c.conn.Write(src.readBuffer[:n])
+		if src.connUdp != nil {
+			n, _, _ := src.connUdp.ReadFromUDP(src.readBuffer)
+			for _, c := range dst {
+				if c.connUdp != nil {
+					c.connUdp.Write(src.readBuffer[:n])
+				}
+			}
 		}
 	}
 }
